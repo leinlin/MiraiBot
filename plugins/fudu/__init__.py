dcd5cd9e65eba4f65c319105802a4138b382af81
leinlin <leinlin@tencent.com>
2021-06-11 13:18:45 +0800

    复读

--- plugins/fudu/__init__.py
+++ plugins/fudu/__init__.py
@@ -5,10 +5,16 @@ from mirai import Mirai, Group, GroupMessage, MessageChain, Image
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 
-last_message = ""
+last_message = None
 @sub_app.receiver(GroupMessage)
-async def fudu(app: Mirai, message: GroupMessage):
-    if message.toString() == last_message:
-        await app.sendGroupMessage(message.sender.group, last_message)
+async def fudu(app: Mirai, group: Group, message: MessageChain):
+    if last_message.toString() == message.toString():
+        image: T.Optional[Image] = message.getFirstComponent(Image)
+        if image and image.url:
+            last_image: T.Optional[Image] = last_message.getFirstComponent(Image)
+            if last_image and last_image.url and last_image.url == image.url:
+                await app.sendGroupMessage(group, message)
+        else
+            await app.sendGroupMessage(group, message)
     else
-        last_message = message.toString()
+        last_message = message
