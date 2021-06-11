a3576ad013519f23089d62ab92411fcb8def5bf2
leinlin <leinlin@tencent.com>
2021-06-11 13:36:33 +0800

    复读

--- plugins/fudu/__init__.py
+++ plugins/fudu/__init__.py
@@ -8,6 +8,7 @@ sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 last_message = None
 @sub_app.receiver(GroupMessage)
 async def fudu(app: Mirai, group: Group, message: MessageChain):
+    global last_message
     if last_message != None and last_message.toString() == message.toString():
         await app.sendGroupMessage(group, message)
     else:
