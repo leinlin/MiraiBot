582851fd1f25455a36418e96dc6124e8349434ac
leinlin <leinlin@tencent.com>
2021-06-15 13:39:54 +0800

    复读

--- plugins/fudu/__init__.py
+++ plugins/fudu/__init__.py
@@ -1,7 +1,7 @@
 import aiohttp
 import lxml.html
 import typing as T
-from mirai import Mirai, Group, GroupMessage, MessageChain, Image
+from mirai import Mirai, Group, GroupMessage, MessageChain, Image, Plain
 from mirai.logger import Event as EventLogger
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
@@ -12,7 +12,12 @@ async def fudu(app: Mirai, group: Group, message: MessageChain):
     global last_message
     if last_message != None and last_message.toString() == message.toString():
         EventLogger.info(f"{message.toString()}消息已复读")
-        await app.sendGroupMessage(group, message)
+        replyArray = []
+        for v in message:
+            if type(v) == Image or type(v) == Plain:
+                replyArray.append(v)
+
+        await app.sendGroupMessage(group, replyArray)
     else:
         if last_message != None:
             EventLogger.info(f"last_message:{last_message.toString()}")
