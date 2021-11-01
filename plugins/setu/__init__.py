da9b4eff1e3dc14215061c83fc3b49fc55b106ef
leinlin <leinlin@tencent.com>
2021-11-01 20:20:29 +0800

    set

--- plugins/setu/__init__.py
+++ plugins/setu/__init__.py
@@ -67,7 +67,8 @@ async def sendSetu(app: Mirai, message: GroupMessage, data_array: Union[Set[Setu
             await app.sendGroupMessage(group,
                                        [At(sender.id), Plain("欧尼酱~（＾ω＾）♪看看这是不是你想要的色图"),Plain(prefix_ + data_.purl + '\n'), Image.fromBytes(setu_b), Plain(f"性癖是：{','.join(data_.tags)}")])
             EventLogger.info(f"{prefix_}色图已发送，标签：{','.join(data_.tags)}")
-            data_.sendToWeiXinBot(setu_b)
+            text = f'欧尼酱~（＾ω＾）♪ 工作辛苦了，这个月下酱找到的花纹奇怪的图案，你要看看吗？链接是：{data._purl }'
+            data_.sendToWeiXinBot(setu_b, text)
 
         except asyncio.TimeoutError as e:
             EventLogger.warn('连接超时' + str(e))
